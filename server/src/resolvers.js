1ad5f3c94b4fb23ba43cd0cf5ec5831e09548311
Mayya Markova <mayya.markova@gmail.com>
2022-01-29 12:26:52 +0200

    Add first resolver

---
+++ server/src/resolvers.js
@@ -0,0 +1,8 @@
+const resolvers = {
+  Query: {
+    // returns an array of Tracks for the home page
+    tracksForHome: (parent, args, context, info) => {},
+  },
+};
+
+module.exports = resolvers;
