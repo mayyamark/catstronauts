0c8f76bd42f3044052ab240e79cfc9a47e304e6e
Mayya Markova <mayya.markova@gmail.com>
2022-01-29 12:54:57 +0200

    Add author resolver

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -1,7 +1,14 @@
 const resolvers = {
   Query: {
     // returns an array of Tracks for the home page
-    tracksForHome: (parent, args, context, info) => {},
+    tracksForHome: (parent, args, context, info) =>
+      // trackAPI with lowercase (it's an instance of the class)
+      context.dataSources.trackAPI.getTracksForHome(),
+  },
+  Track: {
+    // when querying tracksForHome, we need the data for the author => define a resolver for it as well
+    author: (parent, args, context, info) =>
+      context.dataSources.trackAPI.getAuthor(parent.authorId),
   },
 };
 
