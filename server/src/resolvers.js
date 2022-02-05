f9a81b4ca07afed5df9e184f3cf645c271451eb6
Mayya Markova <mayya.markova@gmail.com>
2022-02-05 08:45:03 +0200

    Add server logic for details page

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -3,11 +3,15 @@ const resolvers = {
     // returns an array of Tracks for the home page
     tracksForHome: (_, __, { dataSources }) =>
       dataSources.trackApi.getTracksForHome(),
+    // get a single track by id, for the Track page
+    track: (_, { id }, { dataSources }) => dataSources.trackApi.getTrack(id),
   },
   Track: {
     // returns data for a track's author
     author: ({ authorId }, _, { dataSources }) =>
       dataSources.trackApi.getAuthor(authorId),
+    modules: ({ id }, _, { dataSources }) =>
+      dataSources.trackApi.getTrackModules(id),
   },
 };
 
